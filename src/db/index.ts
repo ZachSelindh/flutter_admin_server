19f9aa1b9a992847ca26ff1c6e5cccea1b2a2dc9
zachSelindh <zachselindh@gmail.com>
2023-02-13 11:37:32 -0600

    Move to new database cluster

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.1gdoj.mongodb.net/?retryWrites=true&w=majority`;
 
-const client: mongoDB.MongoClient = new mongoDB.MongoClient(uri);
+const URL = `mongodb+srv://${process.env.MONGO_DB_FLUTTER_USERNAME}:${process.env.MONGO_DB_FLUTTER_PASSWORD}@cluster0.ypen9fa.mongodb.net/?retryWrites=true&w=majority`;
+
+const client: mongoDB.MongoClient = new mongoDB.MongoClient(URL);
 
 export const dbConnection = client.connect();
